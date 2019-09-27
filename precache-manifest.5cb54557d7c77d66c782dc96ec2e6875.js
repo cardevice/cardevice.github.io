bb1e94c74cf224ce0a3fd32d7de3252a2990a1d0
cardevice <54687883+cardevice@users.noreply.github.com>
2019-09-27 20:24:41 +0300

    Запускаемся

---
+++ precache-manifest.5cb54557d7c77d66c782dc96ec2e6875.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "06dc1e811b41ea976951",
+    "url": "/static/js/main.fec8c1e8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4f23cc292f04490d08f5",
+    "url": "/static/css/2.9cfec7ed.chunk.css"
+  },
+  {
+    "revision": "4f23cc292f04490d08f5",
+    "url": "/static/js/2.1d5a9111.chunk.js"
+  },
+  {
+    "revision": "57407268070ddca4c0a4b990ae414831",
+    "url": "/index.html"
+  }
+];
